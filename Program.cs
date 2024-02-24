4ea88b95b0f57dfab1b0da1ad5695676897a1c53
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-24 21:26:14 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a sentence:");
+        string sentence = Console.ReadLine();
+
+        string reversedSentence = ReverseWords(sentence);
+
+        Console.WriteLine($"Reversed sentence: {reversedSentence}");
+
+        Console.ReadLine(); // Pause the console
+    }
+
+    static string ReverseWords(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = ReverseString(words[i]);
+        }
+        return string.Join(" ", words);
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
